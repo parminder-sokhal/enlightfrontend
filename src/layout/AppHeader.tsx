4c32f78e5a75d2eff0a476213d59e582e2403d6f
PARMINDER SOKHAL <parmindersokhal22@gmail.com>
2025-10-28 17:12:37 +0530

    minc7

--- src/layout/AppHeader.tsx
+++ src/layout/AppHeader.tsx
@@ -85,13 +85,13 @@ const AppHeader: React.FC = () => {
 
           <Link to="/" className="lg:hidden">
             <img
-              className="dark:hidden"
-              src="./images/logo/logo.svg"
+              className="dark:hidden h-10"
+              src="/logo/enlightlogo.png"
               alt="Logo"
             />
             <img
-              className="hidden dark:block"
-              src="./images/logo/logo-dark.svg"
+              className="hidden dark:block h-10"
+              src="/logo/enlightlogo.png"
               alt="Logo"
             />
           </Link>
